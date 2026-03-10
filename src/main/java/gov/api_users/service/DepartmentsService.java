b9a53baaf37f54d0c7781741178ccf5ddb1c70a0
jairolessa <jairo.rafaelcarvalho@gmail.com>
2026-03-10 17:29:51 -0300

    Adicionando métodos para retornar um departamento específico pelo ID e uma lista de departamento

--- src/main/java/gov/api_users/service/DepartmentsService.java
+++ src/main/java/gov/api_users/service/DepartmentsService.java
@@ -1,11 +1,19 @@
 package gov.api_users.service;
 
 import gov.api_users.dto.require.DepartmentCreateDto;
+import gov.api_users.dto.require.DepartmentUpdateListUsersDto;
+import gov.api_users.dto.response.DepartmentDto;
 import gov.api_users.mapper.DepartmentMapper;
 import gov.api_users.model.Department;
+import gov.api_users.model.Users;
 import gov.api_users.repository.DepartmentsRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
 
 @AllArgsConstructor
 @Service
@@ -17,7 +25,27 @@ public class DepartmentsService {
     public void registerDepartment(DepartmentCreateDto departmentCreateDto){
 
         Department entity = departmentMapper.toEntity(departmentCreateDto);
-        System.out.println(entity.toString());
         departmentsRepository.save(entity);
+    }
+
+    public DepartmentDto getDepartment(Long id){
+
+        Department department = getDepartmentById(id);
+
+        return departmentMapper.toResponseDto(department);
+    }
+
+    public List<Department> getDepartments(){
+        return departmentsRepository.findAll();
+    }
+
+    private Department getDepartmentById(Long id){
+        Optional<Department> department = departmentsRepository.findById(id);
+
+        if(department.isEmpty()){
+            throw new RuntimeException("Departamento não encontrado!");
+        }
+
+        return department.get();
     }
 }
