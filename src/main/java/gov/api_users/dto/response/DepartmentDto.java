b3bf3facd592df3994f4d39796bfaeb77c957f31
jairolessa <jairo.rafaelcarvalho@gmail.com>
2026-03-12 01:50:17 -0300

    Fix: correção dos campos do dto de resposta a requisição relacionada ao departamento

--- src/main/java/gov/api_users/dto/response/DepartmentDto.java
+++ src/main/java/gov/api_users/dto/response/DepartmentDto.java
@@ -1,12 +1,21 @@
 package gov.api_users.dto.response;
 
 import gov.api_users.model.UserDepartment;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class DepartmentDto {
 
+    private Long id;
     private String departmentName;
-    private List<UserDepartment> userDepartmentList = new ArrayList<>();
+    private List<UsersSimpleDto> usersList = new ArrayList<>();
 }
