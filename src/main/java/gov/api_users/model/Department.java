652db94384bfef5cd0231dac8aeb2bd27c754d42
jairolessa <jairo.rafaelcarvalho@gmail.com>
2026-03-12 01:46:41 -0300

    Refactor: Importação da biblioteca lombok

--- src/main/java/gov/api_users/model/Department.java
+++ src/main/java/gov/api_users/model/Department.java
@@ -1,10 +1,7 @@
 package gov.api_users.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
